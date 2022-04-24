432c91bed0b1ecc0ebfc3d0599b231d98ca04eae
Jacob Vine <jvine1235@gmail.com>
2022-04-24 01:35:44 -0400

    Cleaned up the code and notes to make it easier to read, cleaned up the hover event listener. Last save before uppload to hosting on github

--- index.js
+++ index.js
@@ -1,40 +1,11 @@
-/*
-key:
-~ = Needs to be completed
-# = in progress of dev.
-! = bugged
-$ = complete and functioning properly
-
-This script will endeavor to do these things:
-$ Create a function that uses a fetch command to pull NASA's picture of the day 
-    API and updates the relevent div elements in the DOM.
-            // Saving the NASA API key and instructions for web request //
-            <!-- Generate API Key
-            Your API key for jvine1235@gmail.com is:
-
-            FCAJPCgyt6BxZj8Ey2QEiO17MVUfkOpPTVKoPkFK
-            You can start using this key to make web service requests. Simply pass your key in the URL when making a web request. Here's an example:
-
-            https://api.nasa.gov/planetary/apod?api_key=FCAJPCgyt6BxZj8Ey2QEiO17MVUfkOpPTVKoPkFK
-            For additional support, please contact us. When contacting us, please tell us what API you're accessing and provide the following account details so we can quickly find you:
-            Account Email: jvine1235@gmail.com
-            Account ID: 67a3c90c-dd2a-42c7-8c4e-339035e35789 -->
-$ Creates a function for a mouse over event on the picture to enlarge the picture.
-$ Creates a function for a click event that reveals the info about the picture.
-$ Creates a function to update the text and img using the NASA API, taking the json as an argument.
-# Have a render function in a DOM content loaded event that calls all css functions, 
-    updates the relevant divs, calls the fetch command, and calls the event functions, 
-    and renders once everything is loaded.
-$ Create a catch as a placeholder in case the API doesn't load or something goes wrong
-*/
-
 
 document.addEventListener('DOMContentLoaded', init);
 
 function init(){
     fetchNasaApi();
-    document.querySelector('img#photo_of_the_day').addEventListener("mouseover", hoverOverZoom)
-    document.querySelector('img#photo_of_the_day').addEventListener("mouseout", hoverOut)
+    document.querySelector('img#photo_of_the_day').addEventListener("mouseover", hoverOverZoom);
+    document.querySelector('div#photo_discription').addEventListener("mouseover", hoverOverZoom);
+    document.querySelector('img#photo_of_the_day').addEventListener("mouseout", hoverOut);
     document.querySelector('img#photo_of_the_day').addEventListener("click", clickForDiscription);
     document.querySelector('div#photo_discription').addEventListener("click", clickForDiscription);
     return;
@@ -108,3 +79,34 @@ function couldNotLoadApi(error){
     return;
     
 };
+
+/*
+key:
+~ = Needs to be completed
+# = in progress of dev.
+! = bugged
+$ = complete and functioning properly
+
+This script will endeavor to do these things:
+$ Create a function that uses a fetch command to pull NASA's picture of the day 
+    API and updates the relevent div elements in the DOM.
+            // Saving the NASA API key and instructions for web request //
+            <!-- Generate API Key
+            Your API key for jvine1235@gmail.com is:
+
+            FCAJPCgyt6BxZj8Ey2QEiO17MVUfkOpPTVKoPkFK
+            You can start using this key to make web service requests. Simply pass your key in the URL when making a web request. Here's an example:
+
+            https://api.nasa.gov/planetary/apod?api_key=FCAJPCgyt6BxZj8Ey2QEiO17MVUfkOpPTVKoPkFK
+            For additional support, please contact us. When contacting us, please tell us what API you're accessing and provide the following account details so we can quickly find you:
+            Account Email: jvine1235@gmail.com
+            Account ID: 67a3c90c-dd2a-42c7-8c4e-339035e35789 -->
+$ Creates a function for a mouse over event on the picture to enlarge the picture.
+$ Creates a function for a click event that reveals the info about the picture.
+$ Creates a function to update the text and img using the NASA API, taking the json as an argument.
+$ Have a render function in a DOM content loaded event that calls all css functions, 
+    updates the relevant divs, calls the fetch command, and calls the event functions, 
+    and renders once everything is loaded.
+$ Create a catch as a placeholder in case the API doesn't load or something goes wrong
+*/
+
